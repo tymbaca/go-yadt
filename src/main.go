c589a7a3c85cea25393ebd58ab5b83544b129f8a
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-06-07 12:56:44 +0200

    ...

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"templater/generator"
+	"time"
 )
 
 var body string = `[
@@ -146,14 +147,157 @@ var body string = `[
 				"address": "Pcholkovo 483"
 			}
 		]
+	},
+	{
+		"filename": "2. Вторник",
+		"pages": [
+			{
+				"organisation": "Naturovo1",
+				"address": "Pcholkovo 481"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo2",
+				"address": "Pcholkovo 482"
+			},
+			{
+				"organisation": "Naturovo3",
+				"address": "Pcholkovo 483"
+			}
+		]
 	}
 ]`
 
 func main() {
+	start := time.Now()
 	// filenames := []string{"go.mod", "go.sum"}
 	// utils.CompressFiles(filenames, "output.zip")
 
 	// filegen.NewFileGenerator(templateFileName: "template.docx", json_data: )
+
 	fmt.Println("hello")
 	fileGenerator, err := generator.New("template.docx", body)
 	if err != nil {
@@ -164,4 +308,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	since := time.Since(start)
+	fmt.Println(since)
 }
