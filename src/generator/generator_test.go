f56d9ab9b0789bfe8f727096535c332b98abee45
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-06-08 12:51:26 +0200

    Added tests

---
+++ src/generator/generator_test.go
@@ -0,0 +1,33 @@
+package generator
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var (
+	waybillFilename   = "tests/waybill.json"
+	templateFilename  = "tests/test_template.docx"
+	outputZipFilename = "tests/output.zip"
+)
+
+func TestGenerateZip(t *testing.T) {
+	body, err := os.ReadFile(waybillFilename)
+	fileGenerator, err := New(templateFilename, body)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	fileCount := len(*fileGenerator.Data)
+	pageCount := 0
+	for _, fileData := range *fileGenerator.Data {
+		pageCount += len(fileData.Pages)
+	}
+
+	t.Log(fmt.Sprintf("Starting generation for %d files, %d pages total...", fileCount, pageCount))
+	err = fileGenerator.GenerateZip(outputZipFilename)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
