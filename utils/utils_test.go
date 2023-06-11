ff0170d826504dcef5877d8612ed71a348694ae5
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-06-11 02:03:10 +0200

    StreamToBytes tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,14 +3,28 @@ package utils
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var (
-	filesToCompress = []string{"tests/image.png", "tests/article.md", "tests/sound.mp3"}
-	outputFilename  = "tests/output.zip"
+	imageFilename   = "tests/image.png"
+	articleFilename = "tests/article.md"
+	soundFilename   = "tests/sound.mp3"
 )
 
+func TestStreamToBytes(t *testing.T) {
+	textBytesReference, _ := os.ReadFile(articleFilename)
+
+	textStream, _ := os.Open(articleFilename)
+	textBytes, _ := StreamToBytes(textStream)
+	assert.Equal(t, textBytes, textBytesReference)
+}
+
 func TestCompressFiles(t *testing.T) {
+	filesToCompress := []string{imageFilename, articleFilename, soundFilename}
+	outputFilename := "tests/output.zip"
+
 	err := CompressFiles(filesToCompress, outputFilename)
 	if err != nil {
 		t.Errorf(err.Error())
